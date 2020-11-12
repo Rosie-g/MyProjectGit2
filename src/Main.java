510f9f7781f79aa6c800a120d9c1fa786c5fa6ec
Rosie-g <ruz0408@gmail.com>
2020-11-11 16:29:13 -0800

    added some changes to Main class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         // Hello beautiful world!!
+        // Hey everyone I just love you!)))
 
     }
 }
